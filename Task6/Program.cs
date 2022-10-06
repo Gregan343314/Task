b0b55a68bf8fd54c8fce09dcf801968a4a6a1c8e
Gregan <343314@gmail.com>
2022-10-06 20:16:39 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿             
+            Console.Write("Введите число : ");
+ 
+            int a = Convert.ToInt32(Console.ReadLine());
+ 
+            if (a % 2 == 0)
+ 
+            {
+ 
+                Console.Write("Введенное число является четным");
+ 
+                Console.Read();
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.Write("Введенное число нечетное");
+ 
+                Console.Read();
+ 
+            }
